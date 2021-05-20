8d8e7d99dc384d0b56da742f59917235bb71df71
Ray (Home) <raymundoruiz91@gmail.com>
2021-05-20 01:43:59 -0500

    Added pagination sample

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,5 @@
 import React, { Component } from 'react';
 import Sidenav from './Sidenav';
-import { Route } from 'react-router-dom';
-import Counter from './Counter';
-import Counters from './Counters';
-import Movies from './Movies';
-import Stuff from './Stuff';
 
 
 class App extends Component {
--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,19 +1,41 @@
 import React, { Component } from 'react'
+import Pagination from './Pagination';
 import { getMovies } from './fakeMovieService'
+import { paginate } from './../scripts/paginate';
+import ListGroup from './listGroup';
+import { getGenres } from './fakeGenreService';
 class Movies extends Component {
   state = {
-    movies: getMovies()
+    movies: [],
+    genres: [],
+    pageSize: 4,
+    currentPage: 1,
+  }
+  componentDidMount() {
+    this.setState({ movies: getMovies(), genres: getGenres() })
   }
 
-  handleDelete = movie =>{
-  
-    const movies = this.state.movies.filter(m=> m._id !== movie._id);
-    this.setState({movies});
+  handleDelete = movie => {
+
+    const movies = this.state.movies.filter(m => m._id !== movie._id);
+    this.setState({ movies });
+  }
+
+  handlePageChange = page => {
+    this.setState({ currentPage: page })
+    console.log(page);
+  }
+
+  handleGenreSelect = genre => {
+    console.log(genre)
   }
 
   render() {
-const {length:count} = this.state.movies
-    if(count == 0 ) return <p>There are no movies in the database.</p>;
+    const { length: count } = this.state.movies
+    const { pageSize, currentPage, movies: allMovies } = this.state;
+    if (count == 0) return <p>There are no movies in the database.</p>;
+
+    const movies = paginate(allMovies, currentPage, pageSize)
 
     return (
       <React.Fragment>
@@ -40,42 +62,51 @@ const {length:count} = this.state.movies
                   <span className="demo-meta-divider">|</span>
                   Process: Main
                 </div>
-              </button> 
+              </button>
               <p>
                 In this demo we create a new Graph (via the{' '}
                 <code>renderChart.js</code> script) and provide a source file
                 <code>input.txt</code> that will generate the graph
               </p>
-              <p>Showing {count} movies in the database</p>
-              <table className="table">
-                <thead>
-                  <tr>
-                    <th>Title</th>
-                    <th>Genre</th>
-                    <th>Stock</th>
-                    <th>Rate</th>
-                    <th></th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {this.state.movies.map(movie => (
-                    <tr key = {movie._id}>
-                      <td>{movie.title}</td>
-                      <td>{movie.genre.name}</td>
-                      <td>{movie.numberInStock}</td>
-                      <td>{movie.dailyRentalRate}</td>
-                      <td>
-                        <button onClick={() => this.handleDelete(movie)} className="demo-button">
-                          Delete
+              <div className="row">
+                <div className="col-2"><ListGroup items={this.state.genres} onItemSelect={this.handleGenreSelect} /></div>
+                <div className="col">
+                  <p>Showing {count} movies in the database</p>
+                  <table className="table">
+                    <thead>
+                      <tr>
+                        <th>Title</th>
+                        <th>Genre</th>
+                        <th>Stock</th>
+                        <th>Rate</th>
+                        <th></th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {movies.map(movie => (
+                        <tr key={movie._id}>
+                          <td>{movie.title}</td>
+                          <td>{movie.genre.name}</td>
+                          <td>{movie.numberInStock}</td>
+                          <td>{movie.dailyRentalRate}</td>
+                          <td>
+                            <button onClick={() => this.handleDelete(movie)} className="demo-button">
+                              Delete
                         </button>
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+
+              </div>
+              <Pagination itemsCount={count} pageSize={pageSize} currentPage={currentPage} onPageChange={this.handlePageChange} />
             </div>
           </div>
         </section>
+
+
       </React.Fragment>
     )
   }
