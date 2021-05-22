b786bd78235549f372fe1eedcc26f5b00fd9d6d7
Ray (Home) <raymundoruiz91@gmail.com>
2021-05-22 04:16:17 -0500

    Working Graphs

--- src/common/Graph.jsx
+++ src/common/Graph.jsx
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
-import { Line } from 'react-chartjs-2';
-import { Bar } from 'react-chartjs-2'
+import { Line, Bar, Pie } from 'react-chartjs-2';
+
 
 
 
 class Graph extends Component {
-    buildData(graphData, title) {
+    buildData(graphData, pieValues) {
 
+       let temp = []
+        if (pieValues !== undefined) {
+            temp = [...pieValues]
+        }else{}
         let data = {
             datasets: [{
                 data: graphData,
@@ -29,7 +33,7 @@ class Graph extends Component {
                 ],
                 borderWidth: 1
             }],
-            labels: []
+            labels: (temp === undefined ? [] : temp)
         }
         return data;
     }
@@ -66,18 +70,29 @@ class Graph extends Component {
 
     render() {
         const { type, data, title } = this.props;
+
         console.log(this.props)
         if (type === 'line') {
 
             return (
                 <div >
-                    <Line data={this.buildData(data, title)} options={this.buildOptions(title)} />
+                    <Line data={this.buildData(data)} options={this.buildOptions(title)} />
                 </div>
             );
-        } else {
+        } else if (type === 'bar') {
             return (
                 <div >
                     <Bar id={title} data={this.buildData(data)} options={this.buildOptions(title)} />
+                </div>
+            )
+        }
+        else if (type === 'pie') {
+            let pieLabels = data.map(x => Object.values(x)[0]);
+            let pieData = data.map(x => Object.values(x)[1]);
+            console.log(pieData, pieLabels)
+            return (
+                <div >
+                    <Pie id={title} data={this.buildData(pieData, pieLabels)} options={this.buildOptions(title)} />
                 </div>
             )
         }
--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -6,35 +6,46 @@ import Table from '../common/Table';
 
 class Analytics extends Component {
   state = {
-    data: [],
+    data1: [],
+    data2: [],
     type: 'bar',
-    buttonState: true,
-    label: "Weekly Page Visits",
+    type2: 'bar',
+    label: "",
     nextSaturday: '',
   }
   formatDate(date) {
-    return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate()
+    return date.getFullYear() + '-' + '0' + (date.getMonth() + 1) + '-' + date.getDate()
   }
 
   parseResults = (response) => {
+    console.log(response)
     const queryResult = response.result.reports[0].data.rows;
-    let data = [];
+    const queryResult2 = response.result.reports[1].data.rows;
+    let data1 = [];
+    let data2 = [];
     const result = queryResult.map((row) => {
       const dateSting = row.dimensions[0];
       const formattedDate = `${dateSting.substring(0, 4).trim().split('\n')}-${dateSting.substring(4, 6).split('\n')}-${dateSting.substring(6, 8).split('\n')}`;
       console.log(formattedDate)
-      data.push({ x: formattedDate, y: row.metrics[0].values[0].trim() })
-
-
-
+      data1.push({ x: formattedDate, y: row.metrics[0].values[0].trim() })
     });
-    console.log(data)
-    this.setState({ data });
+    const result1 = queryResult2.map((row) => {
+      const dateSting = row.dimensions[0].replace('- Bolingbrook, IL', '').trim();
+
+      data2.push({ x: dateSting, y: row.metrics[0].values[0].trim() })
+    });
+    console.log(data1)
+    console.log(data2)
+    data2.sort((firstItem, secondItem) => (secondItem.y - firstItem.y));
+    data2 = data2.splice(1, 8);
+    console.log(data2)
+    this.setState({ data1, data2 });
   };
 
 
 
-  queryReport = () => {
+  queryReport = (sat) => {
+    console.log(sat)
     window.gapi.client
       .request(
         {
@@ -47,8 +58,8 @@ class Analytics extends Component {
                 viewId: "138961309", //enter your view ID here
                 dateRanges: [
                   {
-                    startDate: "10daysAgo",
-                    endDate: "today",
+                    startDate: "6daysAgo",
+                    endDate: this.state.nextSaturday,
                   },
                 ],
                 metrics: [
@@ -58,11 +69,32 @@ class Analytics extends Component {
                 ],
                 dimensions: [
                   {
-                    name: "ga:date",
+                    name: "ga:date"
                   },
                 ],
               },
+              {
+                viewId: "138961309", //enter your view ID here
+                dateRanges: [
+                  {
+                    startDate: "6daysAgo",
+                    endDate: this.state.nextSaturday,
+                  },
+                ],
+                metrics: [
+                  {
+                    expression: "ga:pageviews",
+                  },
+                ],
+                dimensions: [
+                  {
+                    name: "ga:pageTitle"
+                  },
+                ],
+
+              },
             ],
+
           },
         }
       )
@@ -72,20 +104,28 @@ class Analytics extends Component {
   handleClick = () => {
     var d = new Date();
     d.setDate(d.getDate() + (1 + 5 - d.getDay()) % 5);
-    const nextFriday = this.formatDate(d);
+    const nextSaturday = this.formatDate(d);
     var weekAgo = new Date(d)
     weekAgo.setDate(d.getDate() - 6)
     const lastSunday = this.formatDate(weekAgo)
+    const label = 'Total site visits from: ' + lastSunday + " to " + nextSaturday
+    this.setState({ nextSaturday, lastSunday, label }, () => {
+      console.log(this.state.nextSaturday);
+      this.queryReport()
+    });
 
-    this.setState({ nextFriday, lastSunday })
-    console.log(this.state)
-    this.queryReport()
+
+
   }
 
   handleTypeClick = e => {
     this.setState({ type: e.currentTarget.id })
   }
-  columns = [{ path: 'x', label: 'Column1' }, { path: 'y', label: 'Column2' }]
+  handleTypeClick2 = e => {
+    this.setState({ type2: e.currentTarget.id })
+  }
+  columns = [{ path: 'x', label: 'Date' }, { path: 'y', label: 'Total Visits' }]
+  columns2 = [{ path: 'x', label: 'Most Visited' }, { path: 'y', label: 'Yep' }]
   render() {
     return (
       <div>
@@ -95,9 +135,28 @@ class Analytics extends Component {
           <button id="line" onClick={this.handleTypeClick}>Change To Line</button>
           <button id="bar" onClick={this.handleTypeClick}>Change To Bar</button>
         </div>
-        <div> <Table columns={this.columns} data={this.state.data} />
-          <Graph title={this.state.label} type={this.state.type} data={this.state.data} />
+        <div className="row">
+          <div className="col-md-8">
+            <Graph title={this.state.label} type={this.state.type} data={this.state.data1} />
+          </div>
+          <div style={{ alignSelf: 'center' }} className="col-md-4">
+            <Table columns={this.columns} data={this.state.data1} />
+          </div>
+        </div>
 
+        <div>
+          <button id="line" onClick={this.handleTypeClick2}>Change To Line</button>
+          <button id="bar" onClick={this.handleTypeClick2}>Change To Bar</button>
+          <button id="pie" onClick={this.handleTypeClick2}>Change To Pie</button>
+        </div>
+        <div className="row">
+          <div className="col-md-8">
+            <Graph title={this.state.label2} type={this.state.type2} data={this.state.data2} />
+          </div>
+
+          <div style={{ alignSelf: 'center' }} className="col-md-4">
+            <Table columns={this.columns2} data={this.state.data2} />
+          </div>
         </div>
       </div>
     );
